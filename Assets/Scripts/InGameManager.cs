190eea0b94409c5ad818365170f3933b77e4b1d8
ForestLee0513 <woolimlee.dev@gmail.com>
2024-05-16 00:24:16 +0900

    인게임에 필요한 그래픽 에셋 추가

--- Assets/Scripts/InGameManager.cs
+++ Assets/Scripts/InGameManager.cs
@@ -10,13 +10,19 @@ public class InGameManager : MonoBehaviour
     {
         patternData = new BMSMainDataParser(GameManager.Instance.selectedTrack).Pattern;
         
-        for (int i = 0; i < patternData.lines.Length; ++i)
+        // for (int i = 0; i < patternData.lines.Length; ++i)
+        // {
+        //     foreach (Note note in patternData.lines[i].NoteList)
+        //     {
+        //         Debug.Log($"{i}번 째 줄의 {note.Bar} 마디의 {note.Beat}비트 {note.Flag}");
+        //     }
+        // }
+
+        foreach (Note keySound in patternData.bgmKeySoundChannel)
         {
-            foreach (Note note in patternData.lines[i].NoteList)
-            {
-                Debug.Log($"{i}번 째 줄의 {note.Bar} 마디의 {note.Beat}비트 {note.Flag}");
-            }
+            Debug.Log($"{keySound.Bar} 마디의 {keySound.Beat} 박자에 있는 키음은 {keySound.KeySound}");
         }
+    
     }
 
     // Update is called once per frame
